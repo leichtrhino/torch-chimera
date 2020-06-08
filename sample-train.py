448462cef0252b44605461fd167c110bff24e264
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-06-08 22:23:08 +0900

    Update training script

--- sample-train.py
+++ sample-train.py
@@ -23,12 +23,30 @@ from torchchimera.losses import loss_dc_whitend
 from torchchimera.losses import loss_wa
 from torchchimera.losses import loss_csa
 
+import matplotlib.pyplot as plt
+
+def label_matrix(S):
+    batch_size, n_channels, freq_bins, spec_time, _ = S.shape
+    S_abs = S.norm(p=2, dim=-1)
+    p = S_abs.transpose(1, 3).reshape(batch_size, spec_time*freq_bins, n_channels).softmax(dim=-1).cumsum(dim=-1)
+    r = torch.rand(batch_size, spec_time * freq_bins)
+    k = torch.eye(n_channels)[torch.argmin(torch.where(r.unsqueeze(-1) <= p, p, torch.ones_like(p)), dim=-1)]
+    return k
+
+def weight_matrix(X):
+    batch_size, freq_bins, spec_time, _ = X.shape
+    X_abs = X.norm(p=2, dim=-1)
+    weight = X_abs.transpose(1, 2).reshape(batch_size, spec_time*freq_bins)\
+        / X_abs.sum(dim=(1, 2)).clamp(min=1e-16).unsqueeze(-1)
+    return weight
+
 def main():
     # parse arguments and set parameters
-    batch_size = 32
+    batch_size = 8
+    validation_batch_size = 8
     orig_freq = 44100
     target_freq = 16000
-    seconds = 5
+    seconds = 3
     dataset_path = '/Volumes/Buffalo 2TB/Datasets/DSD100'
 
     n_fft = 512
@@ -40,21 +58,41 @@ def main():
     ).shape
 
     # create datasets and dataloaders for train and validation split
-    dataset_train = DSD100(dataset_path, 'Dev', seconds * orig_freq)
+    dataset_train = DSD100(dataset_path, 'Dev', None)
+    dataset_train.transform = RandomTransform(
+        int(dataset_train.get_max_length() / orig_freq * target_freq))
     dataloader_train = torch.utils.data.DataLoader(
-        dataset_train, batch_size=batch_size, num_workers=8, shuffle=True)
-    dataset_validation = torch.utils.data.Subset(
-        DSD100(dataset_path, 'Test', seconds * orig_freq),
-        indices=range(500))
+        dataset_train, batch_size=batch_size, num_workers=2, shuffle=True)
+
+    dataset_validation = DSD100(dataset_path, 'Test', None)
+    dataset_validation.transform = SimpleTransform(
+        int(dataset_validation.get_max_length() / orig_freq * target_freq))
+    dataset_validation = torch.utils.data.Subset(dataset_validation, range(8))
     dataloader_validation = torch.utils.data.DataLoader(
-        dataset_validation, batch_size=batch_size, num_workers=8)
+        dataset_validation, batch_size=validation_batch_size,
+        num_workers=2, shuffle=False)
+
+    def generate_batch(dataloader, waveform_length):
+        for (B, _) in dataloader:
+            batch_size = math.ceil(B.shape[-1] / waveform_length)
+            for batch_i in range(batch_size):
+                batch_end = batch_i == batch_size - 1
+                end = (batch_i + 1) * waveform_length
+                if end <= B.shape[-1]:
+                    yield B[:, :, batch_i*waveform_length:end], batch_end
+                else:
+                    yield torch.cat((
+                        B[:, :, batch_i*waveform_length:],
+                        torch.zeros(B.shape[0], B.shape[1], end-B.shape[2])
+                    ), dim=-1), batch_end
 
     # create networks and load from file if needed
-    initial_epoch = 16 # start at 1
-    train_epoch = 5
+    initial_epoch = 9 # start at 1
+    train_epoch = 1
     loss_function = 'chimera++' # 'chimera++', 'mask', 'wave'
-    input_model = 'model-dc-epoch-15.pth' #'model-dc.pth'
-    output_model = 'model-dc-epoch-20.pth'
+    input_model = f'model-dc-epoch-{initial_epoch-1}.pth'\
+        if initial_epoch > 1 else None
+    output_model = f'model-dc-epoch-{initial_epoch+train_epoch-1}.pth'
     input_misi = None
     output_misi = None
     is_misi_trainable = False
@@ -79,46 +117,49 @@ def main():
         params = list(model.parameters()) + list(misi.parameters())
     optimizer = torch.optim.Adam(params, lr=1e-3)
 
-    stft = lambda x: torch.stft(
-        x.reshape(x.shape[:-1].numel(), seconds * target_freq),
-        n_fft, hop_length, win_length, window=torch.hann_window(n_fft)
-    ).reshape(*x.shape[:-1], freq_bins, spec_time, 2)
+    def forward(model, batch, states=None):
+        stft = lambda x: torch.stft(
+            x.reshape(x.shape[:-1].numel(), seconds * target_freq),
+            n_fft, hop_length, win_length, window=torch.hann_window(n_fft)
+        ).reshape(*x.shape[:-1], freq_bins, spec_time, 2)
+
+        x, s = batch[:, 2, :], batch[:, :2, :]
+        X, S = stft(x), stft(s)
+
+        embd, (mag, phasep, com), states = model(
+            torch.log10(X.norm(p=2, dim=-1).clamp(min=1e-24)),
+            outputs=['mag', 'phasep', 'com'],
+            states=states
+        )
+        states = tuple(state.detach() for state in states)
+
+        # compute loss
+        if loss_function == 'chimera++':
+            Y = label_matrix(S)
+            weight = weight_matrix(X)
+            loss = 0.975 * loss_dc_whitend(embd, Y, weight) \
+                + 0.025 * loss_mi_tpsa(mag, X, S, gamma=2.)
+        elif loss_function == 'mask':
+            loss = 0.5 * loss_mi_tpsa(mag, X, S, gamma=2.) \
+                + 0.5 * loss_csa(com, X, S)
+        elif loss_function == 'wave':
+            Shat = comp_mul(com, X.unsqueeze(1))
+            shat = misi(Shat, x)
+            loss = loss_wa(shat, s)
+        return loss, states
 
     for epoch in range(initial_epoch, initial_epoch+train_epoch):
         sum_loss = 0
         total_batch = 0
+        ave_loss = 0
         last_output_len = 0
+        states = None
         model.train()
-        for step, (batch, _) in enumerate(dataloader_train, 1):
-            batch = torch.stack(
-                [make_random_transform(seconds * target_freq)(b)
-                 for b in batch]
-            )
-            x, s = batch[:, 2, :], batch[:, :2, :]
-            X, S = stft(x), stft(s)
-
-            embd, (mag, phasep, com) = model(
-                torch.log10(X.norm(p=2, dim=-1).clamp(min=1e-12)),
-                outputs=['mag', 'phasep', 'com']
-            )
-
-            # compute loss
-            if loss_function == 'chimera++':
-                S_abs = S.norm(p=2, dim=-1)
-                Y = torch.eye(2)[
-                    torch.argmax(S_abs + 1e-16*torch.rand_like(S_abs), dim=1)
-                    .reshape(batch.shape[0], freq_bins*spec_time)
-                ]
-                loss = 0.975 * loss_dc_whitend(embd, Y) \
-                    + 0.025 * loss_mi_tpsa(mag, X, S, gamma=2.)
-            elif loss_function == 'mask':
-                loss = 0.5 * loss_mi_tpsa(mag, X, S, gamma=2.) \
-                    + 0.5 * loss_csa(com, X, S)
-            elif loss_function == 'wave':
-                Shat = comp_mul(com, X.unsqueeze(1))
-                shat = misi(Shat, x)
-                loss = loss_wa(shat, s)
-
+        for step, (batch, batch_end) in enumerate(
+                generate_batch(dataloader_train, target_freq * seconds), 1):
+            loss, states = forward(model, batch, states)
+            if batch_end:
+                states = None
             sum_loss += loss.item()
             total_batch += batch.shape[0]
             ave_loss = sum_loss / total_batch
@@ -128,7 +169,8 @@ def main():
             optimizer.step()
             sum_grad = sum(
                 torch.sum(torch.abs(p.grad))
-                for n, p in model.named_parameters() if 'blstm' in n and p.grad is not None
+                for n, p in model.named_parameters()
+                if 'blstm' in n and p.grad is not None
             )
             optimizer.zero_grad()
 
@@ -144,34 +186,12 @@ def main():
         with torch.no_grad():
             sum_val_loss = 0
             total_batch = 0
-            for batch, _ in dataloader_validation:
-                batch = torch.stack(
-                    [SimpleTransform(seconds * target_freq)(b)
-                     for b in batch]
-                )
-                x, s = batch[:, 2, :], batch[:, :2, :]
-                X, S = stft(x), stft(s)
-
-                embd, (mag, phasep, com) = model(
-                    torch.log10(X.norm(p=2, dim=-1).clamp(min=1e-12)),
-                    outputs=['mag', 'phasep', 'com']
-                )
-                # compute loss
-                if loss_function == 'chimera++':
-                    S_abs = S.norm(p=2, dim=-1)
-                    Y = torch.eye(2)[
-                        torch.argmax(S_abs+1e-16*torch.rand_like(S_abs), dim=1)
-                        .reshape(batch.shape[0], freq_bins*spec_time)
-                    ]
-                    loss = 0.975 * loss_dc_whitend(embd, Y) \
-                        + 0.025 * loss_mi_tpsa(mag, X, S, gamma=2.)
-                elif loss_function == 'mask':
-                    loss = 0.5 * loss_mi_tpsa(mag, X, S, gamma=2.) \
-                        + 0.5 * loss_csa(com, X, S)
-                elif loss_function == 'wave':
-                    Shat = comp_mul(com, X.unsqueeze(1))
-                    shat = misi(Shat, x)
-                    loss = loss_wa(shat, s)
+            states = None
+            for (batch, batch_end) in generate_batch(
+                    dataloader_validation, target_freq * seconds):
+                loss, states = forward(model, batch, states)
+                if batch_end:
+                    states = None
                 sum_val_loss += loss.item()
                 total_batch += batch.shape[0]
             ave_val_loss = sum_val_loss / total_batch
@@ -200,24 +220,30 @@ def make_random_transform(waveform_length, orig_freq=44100, target_freq=16000):
         orig_freq=orig_freq,
         target_freq=target_freq,
         gain=2**random.gauss(0, 0.1),
-        stretch_rate=2**random.gauss(0, 0.1),
-        shift_rate=2**random.gauss(0, 0.1)
+        stretch_rate=2**random.gauss(0, 0.01),
+        shift_rate=2**random.gauss(0, 0.01)
     )
+
+class CropRight(torch.nn.Module):
+    def __init__(self, waveform_length):
+        super(CropRight, self).__init__()
+        self.waveform_length = waveform_length
+    def forward(self, x):
+        if x.shape[-1] < self.waveform_length:
+            offset_end = self.waveform_length - x.shape[-1]
+            pad_end = torch.zeros(x.shape[0], offset_end)
+            x = torch.cat((x, pad_end), dim=-1)
+        elif x.shape[-1] > self.waveform_length:
+            x = x[:, :self.waveform_length]
+        return x
 
 class SimpleTransform(torch.nn.Module):
     def __init__(self, waveform_length, orig_freq=44100, target_freq=16000):
         super(SimpleTransform, self).__init__()
-        n_fft = 2048
-        hop_length = n_fft // 4
-        n_freq = n_fft // 2 + 1
-        window = torch.hann_window(n_fft)
-        stft = lambda x: torch.stft(x, n_fft, hop_length, window=window)
-        istft = lambda x: torchaudio.functional.istft(
-            x, n_fft, hop_length, window=window)
         self.transform = Compose([
             MixTransform([(0, 1, 2), 3, (0, 1, 2, 3)]),
             Resample(orig_freq=orig_freq, new_freq=target_freq),
-            RandomCrop(waveform_length)
+            CropRight(waveform_length)
         ])
     def forward(self, x):
         return self.transform(x)
@@ -239,11 +265,27 @@ class CustomTransform(torch.nn.Module):
             stft,
             TimeStretch(hop_length, n_freq, stretch_rate * shift_rate),
             istft,
-            Resample(orig_freq=int(orig_freq/shift_rate), new_freq=target_freq),
+            Resample(orig_freq=orig_freq/shift_rate, new_freq=target_freq),
             RandomCrop(waveform_length)
         ])
     def forward(self, x):
         return self.transform(x)
+
+class RandomTransform(torch.nn.Module):
+    def __init__(self, waveform_length, orig_freq=44100, target_freq=16000):
+        super(RandomTransform, self).__init__()
+        self.waveform_length = waveform_length
+        self.orig_freq = orig_freq
+        self.target_freq = target_freq
+    def forward(self, x):
+        return CustomTransform(
+            self.waveform_length,
+            self.orig_freq,
+            self.target_freq,
+            gain=2**random.gauss(0, 0.1),
+            stretch_rate=2**random.gauss(0, 0.01),
+            shift_rate=2**random.gauss(0, 0.01)
+        )(x)
 
 if __name__ == '__main__':
     main()
