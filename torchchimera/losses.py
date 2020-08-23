7042766c173a64ac9e77f1dda9ca9decc96d2bc5
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-08-23 13:13:18 +0900

    Update permutation-free loss functions

--- torchchimera/losses.py
+++ torchchimera/losses.py
@@ -30,6 +30,18 @@ def loss_dc_whitend(embd, label, weight=None):
         dim=0
     ))
 
+def permutation_free(loss_function):
+    def _loss_function(*args, **kwargs):
+        return sum(
+            min(
+                loss_function(
+                    *[a.unsqueeze(0) for a in (torch.stack(p), *_args[1:])],
+                    **kwargs
+                ) for p in permutations(_args[0])
+            ) for _args in zip(*args)
+        )
+    return _loss_function
+
 # loss functions for mask inference head
 # mask: (batch_size, n_channels, freq_bin, time)
 # source: (batch_size, n_channels, freq_bin, time, 2)
@@ -41,10 +53,7 @@ def loss_mi_msa(mask, mixture, sources):
     phase_comp = lambda X: torch.atan2(*X.split(1, dim=-1)[::-1]).squeeze()
     abs_X = abs_comp(mixture)
     abs_S = abs_comp(sources)
-    return sum(min(
-        torch.sum((torch.stack(M) * _abs_X - _abs_S) ** 2)
-        for M in permutations(_mask)
-    ) for _mask, _abs_X, _abs_S in zip(mask, abs_X, abs_S))
+    return torch.sum((mask * abs_X - abs_S) ** 2)
 
 def loss_mi_tpsa(mask, mixture, sources, gamma=1, L=1):
     C = mask.shape[1]
@@ -63,15 +72,9 @@ def loss_mi_tpsa(mask, mixture, sources, gamma=1, L=1):
     )
 
     if L == 1:
-        return sum(min(
-            torch.sum(torch.abs(torch.stack(M) * _abs_X - _spectrum))
-            for M in permutations(_mask)
-        ) for _mask, _abs_X, _spectrum in zip(mask, abs_X, spectrum))
+        return torch.sum(torch.abs(mask * abs_X - spectrum))
     elif L == 2:
-        return sum(min(
-            torch.sum((torch.stack(M) * _abs_X - _spectrum) ** L)
-            for M in permutations(_mask)
-        ) for _mask, _abs_X, _spectrum in zip(mask, abs_X, spectrum))
+        return torch.sum((mask * abs_X - spectrum) ** L)
     else:
         raise NotImplementedError()
 
@@ -79,37 +82,7 @@ def loss_mi_tpsa(mask, mixture, sources, gamma=1, L=1):
 # source_pred: (batch_size, n_channels, waveform_length)
 # source_true: (batch_size, n_channels, waveform_length)
 def loss_wa(source_pred, source_true):
-    return sum(min(
-            torch.sum(torch.abs(torch.stack(s) - _source_true))
-            for s in permutations(_source_pred)
-        ) for _source_pred, _source_true in zip(source_pred, source_true))
-
-# loss for cross entropy phasebook
-# phase_prob_pred: (batch_size, n_channels, freq_bin, time, book_size)
-# phase_true: (batch_size, n_channels, freq_bin, time, 2) (complex) or
-#             (batch_size, n_channels, freq_bin, time) (phase in radias)
-# phasebook: phasebook
-def loss_ce_phase(phase_prob_pred, phase_true, phasebook):
-    if len(phase_true.shape) == 5: # treat as complex number
-        phase_true_norm = phase_true\
-            / torch.sqrt(torch.sum(phase_true**2, dim=-1).clamp(min=1e-12))\
-            .unsqueeze(-1)
-        phase_true = torch.atan2(*phase_true_norm.split(1, dim=-1)[::-1])
-        phase_true = phase_true.squeeze(-1)
-    phase_ref_idx = torch.argmin(
-        torch.min(
-            (phasebook.view(1,1,1,1,-1)-phase_true.unsqueeze(-1)) % (2*pi),
-            (phase_true.unsqueeze(-1)-phasebook.view(1,1,1,1,-1)) % (2*pi)
-        ),
-        dim=-1
-    )
-    return sum(min(
-        torch.sum(-torch.log(
-            torch.stack(p).gather(-1, _ref_idx.unsqueeze(-1))
-            .squeeze(-1).clamp(min=1e-36)
-        ))
-        for p in permutations(_prob)
-    ) for _prob, _ref_idx in zip(phase_prob_pred, phase_ref_idx))
+    return torch.sum(torch.abs(source_pred - source_true))
 
 # loss function for spectrogram (complex domain)
 # com_pred: (batch_size, n_channels, freq_bin, spec_time, 2)
@@ -126,14 +99,8 @@ def loss_csa(com_pred, mixture, sources, L=1):
     source_pred = comp_mul(com_pred, mixture.unsqueeze(1))
 
     if L == 1:
-        return sum(min(
-            torch.sum(abs_comp(torch.stack(s) - _sources))
-            for s in permutations(_source_pred)
-        ) for _source_pred, _sources in zip(source_pred, sources))
+        return torch.sum(abs_comp(source_pred - sources))
     elif L == 2:
-        return sum(min(
-            torch.sum(abs_comp(torch.stack(s) - _sources) ** L)
-            for s in permutations(_source_pred)
-        ) for _source_pred, _sources in zip(source_pred, sources))
+        return torch.sum(abs_comp(source_pred - sources) ** 2)
     else:
         raise NotImplementedError()
